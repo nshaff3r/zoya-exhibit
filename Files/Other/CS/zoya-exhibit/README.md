a5054932205f74850d2f99444480b7813d0640d8
Nolan Shaffer <nshaff3r@gmail.com>
2026-01-30 15:04:09 -0600

    File upload

---
+++ Files/Other/CS/zoya-exhibit/README.md
@@ -0,0 +1,6 @@
+# hunkydory
+
+React with motion and some fun scroll-based animations ❤️.
+<br/>
+[www.hunkydory.online](http://www.hunkydory.online/)
+# zoya-exhibit
---
+++ Files/Other/CS/zoya-exhibit/public/content.js
@@ -0,0 +1,76 @@
+export const headline = "Zoya Cerkassky Offers a Response to Our Global Political Crisis: Sex."
+export const subhead = "In her first exhibition since a series of protests interrupted her exhibit about October 7th, the edgy Zoya Cherkassky gave a comical, defiant, and grim answer."
+export const byline = "Written and Developed by <a href='https://chicagomaroon.com/staff_name/nolan-shaffer'>Nolan Shaffer</a>."
+export const p1 = [
+    "What does an appropriate artistic response to our global political crisis look like? And what does that tell us about the society we live in? For Zoya Chernkassky, it’s all rather explicit. The Ukrainian-Israeli artist, who’s spent much of her life in the context of conflict–she grew up in Soviet-controlled Kyiv before moving to Israel in 1991–answered with sex, and lots of it, in her exhibition <i>Zoya Cherkassky: The Global Political Crisis</i> at the Neubauer Collegium this past season.",
+    "It’s a strange feeling to walk into an exhibit titled “The Global Political Crisis” and be greeted by a deluge of pornography: mugs with breasts, a gun-shaped penis, large paintings of nudity, and many, many, positions of intercourse."
+]
+
+export const s1 = [
+    "But the first thing that catches your eye, placed squarely above a fireplace in the center of the room, is actually a site-specific marquee proclaiming “LOVE” in English, Hebrew, and Arabic (there is a small, clay-shaped vulva on the bottom).",
+    "It is a moment of clarity in a compelling, defiant exhibition.",
+    "This, indeed, is more than about sex."
+]
+export const p2 = [
+    "In Israel, Zoya (she is known by her first name) has developed quite the <a href='https://www.theatlantic.com/magazine/archive/2024/11/zoya-israeli-art-paintings-war/679957/'>reputation</a>. She’s an edgy, leading contemporary artist who’s sarcastic, satirical, and not afraid to paint what she sees. She’s no stranger to criticizing Israeli society. Much of her work has been influenced by her experience of being an outsider–as a Jew in Kyiv, an immigrant in Israel, and there, a non-Jew by rabbinical standards (her mother is not Jewish).",
+]
+
+export const s2 = [
+    "And so it must have been a surprise to her, though not an entirely unfamiliar one, when at event for her previous exhibition",
+    "–a searing collection of Beckmann-esque paintings about October 7th at the Jewish Museum in New York–",
+    "she was interrupted by four waves of protesters decrying the “manufacturing [of] consent for genocide”.",
+    "Zoya produced nearly all the works for this exhibition in the time since then.",
+    "It seems clear that it is something of a response."
+]
+
+export const p3 = [
+    "At first sight, the exhibition’s message seems to be one of “make love not war”. Curator Dieter Roelstraete said Zoya jokingly called it the most pacifist gesture since John Lennon recorded “Imagine” in 1971."
+]
+
+export const s3 = [
+    "<i>Capital after Globalism</i> features a nude woman lying on grass comically reading a book with “Capital After Globalism” painted humorously large on the cover.",
+    "In the background, two women frolic in apparent reference to Paul Cézanne’s Bathers.",
+    "With bold colors, large brushstrokes, and transparent staging, Zoya’s works are best read like political cartoons.",
+    "This peaceful, playful painting seems to say “no matter your politics, we’re all naked. And sometimes you just want to lie or frolic in the sun.”",
+    "“Or have sex in the kitchen.”",
+    "“Or in a cabin. Can’t we just do that?”"
+]
+
+export const p4 = [
+    "But it would be a disservice to Zoya’s satirical nuance to flatten the exhibition’s message to mere pacifism.",
+    "There is, too, an underlying protest against a stifling art world. “Zoya Cherkassky”, Roelstraete told me, “is the kind of artist who in recent years has found or has felt that a lot of [the freedoms in art] are under pressure”.",
+    "Zoya’s response is given in bold and brazen proportions."
+]
+
+export const s4 = [
+    "At the center of the exhibition is a large table with dozens of erotic drawings, sculptures, and found objects.",
+    "A pair of surreal, ceramic spherical breasts impaled on a tree.",
+    "An androgynous couple lying blissfully under a poster that reads “GLOBALIZE THE INTIFADA”.",
+    "A drawing of a woman on a nude beach holding a sign proclaiming “YOU ARE HEADED FOR HELL!” surrounded by an exuberance of nudity and sex.",
+    "An exact recreation of Sylvia Sleigh’s <i>The Turkish Bath</i> with her husband in the foreground.",
+]
+
+export const p5 = [
+    "To callously skirt around violence and bodily reality (the lack of gore in the violently impaled breasts, the perfection of bodies devoid of wrinkles and sags) while at the same time titling and referencing the global political crisis, is to stick two middle fingers up at an art world and an audience that demand a statement from her. It is bitingly edgy and rather bleak.",
+    "Has our appetite for discourse been so trampled that Zoya’s only response can be one that deals in vulgarities?",
+    "The exhibition is playful and humorous, yes, but the sheer quantity of pornographic material betrays a kind of Freudian trauma. A channeling of emotion, rage, and exhaustion into sex. One imagines Zoya in her studio furiously making these works against the backdrop of the protests and the events that followed.",
+    "That is not to say there are not quieter, explicitly emotional moments."
+]
+
+export const s5 = [
+    "In the exhibition’s cover painting, The Loner, a naked man sits next to a table with a book in his hand.",
+    "He is not reading but rather looking directly at the viewer, with a mournful, contemplative look in his eyes. It seems he has little desire to do anything.",
+    "A tea cake sits untouched, its accompanying tea pot useless without a cup in sight.",
+    "He is out of answers, and with his head resting on his fist, he can do nothing but stare out at the viewer (and from a ten-foot-tall announcement of the exhibit, at the University of Chicago), as if awaiting a response.",
+    "Through a window behind him, a single apartment in the night has its lights on. Is there another man, out of answers, in that one?",
+    "And through its window, the apartment of another’s?",
+    "Then on the wall next to this piece, a woman with a pair of buoyant breasts cooks a pair of sunny-side-up eggs.",
+    "The embellished roundness of her body is set against a comically ridiculous level of geometric crispness.",
+    "The whole thing has the innuendo and humor of a giggling middle schooler.",
+    "Next to the man, it’s disturbing, hilarious, and strangely defiant."
+]
+
+export const p6 = [
+    "It’s never been easy to be an artist who satirizes culture and politics. It’s now nearly impossible. Would another institution, without the Collegium's commitment to inquiry and intellectual creativity, have hosted an exhibition like this? Maybe not."
+]
+export const p7 = "And so for an artist with a near life-long experience of cultural critique, the only response to the global political crisis is now sex, sex, and more sex. But sharp satirists are bellwethers of their society, and they must make do. “It's just kind of incredible”, Roelstraete told me, “to see firsthand [how] art, even as silly as this, can stir the soul.”"
